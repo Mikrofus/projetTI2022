55aaaf8a29beda7f1e025106475e973fe6700c89
Kyllian Alland <kyllian1709@gmail.com>
2022-11-15 10:45:10 +0100

    connection + inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PageDaccueilComponent } from './page-daccueil/page-daccueil.component';
+import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageDaccueilComponent
+    PageDaccueilComponent,
+    LoginComponent,
+    InscriptionComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/inscription/inscription.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.css']
+})
+export class InscriptionComponent implements OnInit {
+  formSignIn1: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  SignIn() {
+
+  }
+}
