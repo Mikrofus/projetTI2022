b2386c9b0ffe8ddfc88ce3cd8b6423252d03b2f7
Kyllian Alland <kyllian1709@gmail.com>
2022-11-15 20:05:58 +0100

    validation inscription

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-inscription',
@@ -6,14 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-  formSignIn1: any;
+  formSignIn1: FormGroup = this._fb.group({
+    pseudo:this._fb.control("",[Validators.required,Validators.minLength(3)]),
+    mail:this._fb.control("",[Validators.required, Validators.email]),
+    password:this._fb.control("",[Validators.required,Validators.minLength(6)])
+  })
 
-  constructor() { }
+  constructor(private _fb : FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   SignIn() {
+  }
 
+  get pseudoControl(){
+    return this.formSignIn1.controls['pseudo'];
+  }
+  get mailControl(){
+    return this.formSignIn1.controls['mail'];
+  }
+  get passwordControl(){
+    return this.formSignIn1.controls['password'];
   }
 }
