a8758d5c449bc62e4df9dc5b403aa381f2e86dca
Fabio <fabiodivito765@hotmail.com>
2022-11-19 20:26:14 +0100

    barre navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PageDaccueilComponent } from './page-daccueil/page-daccueil.component';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageDaccueilComponent
+    PageAccueilComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
