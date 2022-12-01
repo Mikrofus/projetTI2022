8ecc715cc754e75c7d187580a55892037fe2b2b4
bartlebg <la188971@student.helhe.be>
2022-12-01 10:06:33 +0100

    Formulaire add auction

---
+++ src/app/form-add-auction/form-add-auction.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-form-add-auction',
+  templateUrl: './form-add-auction.component.html',
+  styleUrls: ['./form-add-auction.component.css']
+})
+export class FormAddAuctionComponent {
+
+  ngOnInit(){
+  }
+
+  onFormSubmit(auctionForm:NgForm){
+    console.log(auctionForm)
+  }
+
+  resetForm(auctionForm: NgForm){
+    auctionForm.resetForm();
+  }
+
+}
