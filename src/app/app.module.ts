e3803cf2351bc54b58502c15957bb014195db231
Fabio <fabiodivito765@hotmail.com>
2022-12-02 20:25:30 +0100

    Generation des encheres grace a la base de données

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { ProductHubComponent } from './product-hub/product-hub.component';
+import { AuctionHubComponent } from './auction-hub/auction-hub.component';
+import { AuctionListComponent } from './auction-hub/auction-list/auction-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PageAccueilComponent,
+    ProductHubComponent,
+    AuctionHubComponent,
+    AuctionListComponent,
 
   ],
   imports: [
