bf1df88f60cabe9a15efed7b59b61823e6a2724a
bartlebg <la188971@student.helhe.be>
2022-12-13 14:12:24 +0100

    création du menu pour enchérir

---
+++ src/app/menu-auction/menu-auction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-auction',
+  templateUrl: './menu-auction.component.html',
+  styleUrls: ['./menu-auction.component.css']
+})
+export class MenuAuctionComponent {
+
+}
