26ea71903d145deb6340b7b2ffa39b5437b3a697
Valentin Dejean <la209054@student.helha.be>
2022-12-13 22:11:48 +0100

    Résoudre problème

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import {HttpClientModule} from "@angular/common/http";
     AppRoutingModule,
     RouterLink,
     RouterLinkActive,
-    HttpClientModule
+    HttpClientModule,
     FormsModule,
 
   ],
