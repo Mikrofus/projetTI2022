f659c343ef47d1210abc6297f203a651bddde4cc
bartlebg <la188971@student.helhe.be>
2022-12-17 14:59:01 +0100

    creation du userHub

---
+++ src/app/user-hub/user-hub.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-hub',
+  templateUrl: './user-hub.component.html',
+  styleUrls: ['./user-hub.component.css']
+})
+export class UserHubComponent {
+
+}
