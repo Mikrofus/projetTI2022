d56d5c002e9efc4ced374a331a0f07b12cd3c526
bartlebg <la188971@student.helhe.be>
2022-12-17 15:21:00 +0100

    misse en place de user.service.ts et de user-hub.component.ts

---
+++ src/app/user-hub/user.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {DtoInputUser} from "./dtos/dto-input-user";
+import {HttpClient} from "@angular/common/http";
+import {DtoOutputCreateUser} from "./dtos/dto-output-user";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private static readonly ENTRY_POINT = environment.apiUrl + "/users";
+
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  fetchAll(): Observable<DtoInputUser[]> {
+    return this._httpClient.get<DtoInputUser[]>(UserService.ENTRY_POINT);
+  }
+
+  fetchById(id: number): Observable<DtoInputUser> {
+    return this._httpClient.get<DtoInputUser>(`${UserService.ENTRY_POINT}/${id}`);
+  }
+
+  create(dto: DtoOutputCreateUser): Observable<DtoInputUser> {
+    return this._httpClient.post<DtoInputUser>(UserService.ENTRY_POINT, dto);
+  }
+
+
+}
