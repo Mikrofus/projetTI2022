b2f4975f9000c18eeec18a1761bcb4b8943a0cf9
bartlebg <la188971@student.helhe.be>
2022-12-17 15:00:12 +0100

    modification

--- src/app/menu-auction/menu-auction.component.ts
+++ src/app/menu-auction/menu-auction.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {DtoInputAuction} from "../auction-hub/dtos/dto-input-auction";
+import {AuctionService} from "../auction-hub/auction.service";
+
 
 @Component({
   selector: 'app-menu-auction',
   templateUrl: './menu-auction.component.html',
   styleUrls: ['./menu-auction.component.css']
 })
-export class MenuAuctionComponent {
+export class MenuAuctionComponent implements OnInit{
+
+  auction: DtoInputAuction | null = null;
+  found: boolean = false;
+
+  constructor(private _auctionService: AuctionService,
+              private _route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
