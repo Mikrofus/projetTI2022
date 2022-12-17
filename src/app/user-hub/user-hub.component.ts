616c34cd8fadba81e1d9dedf8cab3bd11cc48f4d
bartlebg <la188971@student.helhe.be>
2022-12-17 15:21:00 +0100

    misse en place de user.service.ts et de user-hub.component.ts

--- src/app/user-hub/user-hub.component.ts
+++ src/app/user-hub/user-hub.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {DtoInputUser} from "./dtos/dto-input-user";
+import {UserService} from "./user.service";
+import {ActivatedRoute} from "@angular/router";
+import {DtoOutputCreateUser} from "./dtos/dto-output-user";
 
 @Component({
   selector: 'app-user-hub',
   templateUrl: './user-hub.component.html',
   styleUrls: ['./user-hub.component.css']
 })
-export class UserHubComponent {
+export class UserHubComponent implements OnInit{
+  users: DtoInputUser[] = [];
+
+  constructor(private _userService: UserService,
+              private _route: ActivatedRoute) {
+  }
+
+  ngOnInit() : void{
+    this.fetchAll();
+  }
+
+  private fetchAll(){
+    this._userService
+      .fetchAll()
+      .subscribe(users=>this.users = users);
+  }
+
+  create(dto : DtoOutputCreateUser){
+    this._userService.create(dto).subscribe(user => this.users.push(user));
+  }
 
 }
