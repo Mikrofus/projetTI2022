19f670bfeeb65af21911f6c5bbf6f81660b95870
bartlebg <la188971@student.helhe.be>
2022-12-18 11:09:05 +0100

    ajout du menu user créer par bryan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {AppRoutingModule} from "./app-routing.module";
 import { FormAddAuctionComponent } from './form-add-auction/form-add-auction.component';
 import {HttpClientModule} from "@angular/common/http";
 import {MenuAuctionComponent} from "./menu-auction/menu-auction.component";
+import { MenuUserComponent } from './menu-user/menu-user.component';
 
 
 
@@ -27,7 +28,8 @@ import {MenuAuctionComponent} from "./menu-auction/menu-auction.component";
     InscriptionComponent,
     PageAccueilComponent,
     FormAddAuctionComponent,
-    MenuAuctionComponent
+    MenuAuctionComponent,
+    MenuUserComponent
   ],
   imports: [
     BrowserModule,
