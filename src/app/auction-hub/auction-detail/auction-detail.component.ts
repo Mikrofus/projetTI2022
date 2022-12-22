7d06d38ff61c00dd51de68ca9d69413eb12f338f
BryanMach <la208618@student.helha.be>
2022-12-22 14:16:03 +0100

    Timer Methode rajouter

--- src/app/auction-hub/auction-detail/auction-detail.component.ts
+++ src/app/auction-hub/auction-detail/auction-detail.component.ts
@@ -55,4 +55,22 @@ export class AuctionDetailComponent {
   updatePrice(price: number) {
     this.price = price;
   }
+
+  countDownDate = new Date("Dec 24, 2022 14:00:00").getTime();
+  demo: any;
+
+  x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = this.countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    this.demo = `${days}:${hours}:${minutes}:${seconds}`;
+
+    if (distance < 0) {
+      clearInterval(this.x);
+      this.demo = "EXPIRED";
+    }
+  });
 }
