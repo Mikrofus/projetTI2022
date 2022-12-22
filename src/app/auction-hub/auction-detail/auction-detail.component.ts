27d0385d1d230e839d5486639df3276004b48de4
BryanMach <la208618@student.helha.be>
2022-12-22 21:19:36 +0100

    ajout du timer qui se base sur la date finale de l'enchere dans la db dans le auction detail

--- src/app/auction-hub/auction-detail/auction-detail.component.ts
+++ src/app/auction-hub/auction-detail/auction-detail.component.ts
@@ -1,7 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {AuctionService} from "../auction.service";
-import {DtoInputAuction} from "../dtos/dto-input-auction";
 import {DtoOutputPatchAuction} from "../dtos/dto-output-patch-auction";
 
 
@@ -14,7 +13,7 @@ export class AuctionDetailComponent {
   auction: any;
   price: number = 0;
   auctionPatch: DtoOutputPatchAuction = {id: 0, price: 0, idUserBid: 1}
-  @Input() auctions: DtoInputAuction[] = [];
+  timer: any;
 
   constructor(private service: AuctionService, private route: ActivatedRoute) {
 
@@ -54,21 +53,22 @@ export class AuctionDetailComponent {
       this.price = price;
   }
 
-  countDownDate = new Date("Dec 22, 2022 16:23:00").getTime();
-  demo: any;
+
+
 
   x = setInterval(() => {
+    let countDownDate = new Date(this.auction.timer).getTime();
     let now = new Date().getTime();
-    let distance = this.countDownDate - now;
+    let distance = countDownDate - now;
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    this.demo = `${days}:${hours}:${minutes}:${seconds}`;
+    this.timer = `${days}:${hours}:${minutes}:${seconds}`;
 
     if (distance < 0) {
       clearInterval(this.x);
-      this.demo = "EXPIRED";
+      this.timer = "EXPIRED";
     }
   });
 }
